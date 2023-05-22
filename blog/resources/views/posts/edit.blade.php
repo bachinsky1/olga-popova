7a62e66bcd437d67ee7af9622853a77528414350
V. Bachinsky <bachinsky1@gmail.com>
2023-05-22 23:39:49 +0300

    Removed unused

--- blog/resources/views/posts/edit.blade.php
+++ blog/resources/views/posts/edit.blade.php
@@ -72,36 +72,5 @@
     selector: 'textarea#description'
   });
 </script>
-<!--         
-<div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="POST" action="{{ route('posts.update',$post->id) }}">
-                        @csrf
-                        @method('put')
-                        <div class="mb-6">
-                            <label class="block">
-                                <span class="text-gray-700">Title</span>
-                                <input type="text" name="title" class="block w-full @error('title') border-red-500 @enderror mt-1 rounded-md" placeholder="" value="{{old('title',$post->title)}}" />
-                            </label>
-                            @error('title')
-                            <div class="text-sm text-red-600">{{ $message }}</div>
-                            @enderror
-                        </div> 
-                        <div class="mb-6">
-                            <label class="block">
-                                <span class="text-gray-700">Description</span>
-                                <textarea class="block w-full mt-1 rounded-md" name="description" rows="3">{{ $post->description}}</textarea>
-                            </label>
-                            @error('description')
-                            <div class="text-sm text-red-600">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <button type="submit" class="text-white bg-blue-600  rounded text-sm px-5 py-2.5">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> -->
+
 @overwrite
